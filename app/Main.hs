6cce5087bcb2734177f9adc5bbd286dc08f374d0
Anton Logvinenko <anton.logvinenko@gmail.com>
2019-04-04 22:49:26 +0300

    Added twitter API and creds storage: draft and messy code

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,26 @@
 
 module Main where
 
-import Lib
+import qualified Data.Aeson as Aeson
 import qualified Data.Text as T
-import Control.Monad (forever)
-import Network.Wreq (defaults, get, responseBody, responseHeaders)
-import Network.Wreq.Types (checkResponse, Options)
-import Control.Lens ((^.))
 import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as S8
+import Network.Wreq (defaults, get, postWith, responseBody, responseStatus, statusCode)
+import Network.Wreq.Types (checkResponse, Options, auth, Auth(OAuth1))
+import Network.Wreq.Lens (param)
+import Control.Lens ((^.), (&), (.~))
 import Text.HTML.DOM (parseLBS)
-import Text.XML.Cursor (Cursor, attributeIs, attribute, element, fromDocument, child,
-                        ($//), (&|), (&//), (>=>))
-
+import Text.XML.Cursor (Cursor, attributeIs, attribute, element, fromDocument, ($//), (&|), (>=>))
+import Control.Monad.Trans.Except
+import Control.Monad.IO.Class
+import Control.Monad (forever)
 
 opts :: Options
 opts = defaults {checkResponse = Just $ \_ _ -> return ()}
 
+
+-- Wikipedia API
 getRandomArticle :: String -> IO BSL.ByteString
 getRandomArticle lang = do
   response <- get $ "http://" ++ lang ++ ".wikipedia.org/wiki/Special:Random"
@@ -37,6 +42,36 @@ processData =  T.unpack . T.concat
 
 getRandomUrl :: String -> IO String
 getRandomUrl lang = getUrlFromPage <$> getRandomArticle lang
+
+
+
+-- Twitter API
+secretPath :: String
+secretPath = "./secret"
+emptyBody :: Aeson.Value
+emptyBody = Aeson.toJSON (Nothing :: Maybe String)
+
+getOAuthCreds :: FilePath -> ExceptT String IO Auth
+getOAuthCreds path = do
+  c <- liftIO $ Aeson.decodeStrict <$> BS.readFile path
+  let cred c n = S8.pack $ c !! n
+  case (c :: Maybe [String]) of
+    Nothing -> throwE ""
+    (Just c) -> return $ OAuth1 (cred c 0) (cred c 1) (cred c 2) (cred c 3)
+
+verify r msg =
+  let code = r ^. responseStatus ^. statusCode
+  in if code == 200 then return () else throwE  ""
+
+tweet :: String -> Auth -> ExceptT String IO ()
+tweet text authCreds = do
+  response <- liftIO $ postWith
+              (opts {auth = Just authCreds} & param "status".~ [T.pack text])
+              "https://api.twitter.com/1.1/statuses/update.json"
+              emptyBody
+  verify response $ "Posting: " ++ text
+
+
 
 main :: IO ()
 main = getRandomUrl "en" >>= print
